0a1ea2978050604e45fcc50b6580f8aaa17f4231
Alex Trotsenko <olexii.trotsenko@gmail.com>
2016-10-04 16:46:08 +0200

    added APP_PLATFORM=android-23 in order to avoid possible app's crash on Android 6 with UnsatisfiedLinkError

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,1 +1,4 @@
 APP_ABI := all
+# If APP_PLATFORM set to 24 or less then 23 - then app is crashing on Android 6 with "java.lang.UnsatisfiedLinkError: dlopen failed: cannot locate symbol "__aeabi_memclr4" referenced by "/data/app/.../lib/arm/libsqlcipher.so"""
+# See https://github.com/android-ndk/ndk/issues/126 for details.
+APP_PLATFORM=android-23
